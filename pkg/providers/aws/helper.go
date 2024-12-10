97946340e97294f4910583238375e54a1f52db05
Javier Carrillo <jamaca1410@gmail.com>
2024-12-09 23:25:36 -0400

    chore: improve var management and errors on helper file

--- pkg/providers/aws/helper.go
+++ pkg/providers/aws/helper.go
@@ -21,29 +21,39 @@ type GetUserAccessKeyInputs struct {
 func GetUserAccessKey(input GetUserAccessKeyInputs) ([]UserData, error) {
 	var usersData []types.User
 	var err error
+
 	if input.UserName != "" {
 		usersData = []types.User{{UserName: aws.String(input.UserName)}}
 	} else {
 		usersData, err = input.Client.ListUsers(input.MaxUsers)
 		if err != nil {
-			log.Errorf("Couldn't list users. Here's why: %v\n", err)
+			log.Errorf("Couldn't list users: %v", err)
 			return nil, err
 		}
 	}
 
-	var userKeyData []UserData
+	var (
+		userKeyData []UserData
+		mu          sync.Mutex
+		wg          sync.WaitGroup
+		errors      []error
+	)
 
-	wg := sync.WaitGroup{}
 	wg.Add(len(usersData))
 
 	for _, user := range usersData {
-		go func() {
+		go func(user types.User) {
 			defer wg.Done()
 			keyData, err := input.Client.ListAccessKeys(*user.UserName, input.TimeZone, input.Expired, input.Age)
 			if err != nil {
-				log.Errorf("Couldn't list users. Here's why: %v\n", err)
+				log.Errorf("Couldn't list access keys for user %s: %v", *user.UserName, err)
+				mu.Lock()
+				errors = append(errors, err)
+				mu.Unlock()
 				return
 			}
+
+			mu.Lock()
 			if input.Expired {
 				if keyData.Keys != nil {
 					userKeyData = append(userKeyData, keyData)
@@ -51,15 +61,20 @@ func GetUserAccessKey(input GetUserAccessKeyInputs) ([]UserData, error) {
 			} else {
 				userKeyData = append(userKeyData, keyData)
 			}
-		}()
+			mu.Unlock()
+		}(user)
 	}
 
 	wg.Wait()
+	if len(errors) > 0 {
+		return nil, errors[0] // Returning the first error as an example
+	}
 
-	// Sort the slice by age in descending order
-	sort.Slice(userKeyData, func(i, j int) bool {
-		return userKeyData[j].(UserAccessKeyData).UserName > userKeyData[i].(UserAccessKeyData).UserName
-	})
+	if input.UserName != "" {
+		sort.Slice(userKeyData, func(i, j int) bool {
+			return userKeyData[j].(UserAccessKeyData).UserName > userKeyData[i].(UserAccessKeyData).UserName
+		})
+	}
 
 	return userKeyData, nil
 }
